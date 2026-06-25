d8fadc4426cdff72bd2e8e2d7e7e4e673f2e9953
Sara Cervantes <cervantessaralucia4@gmail.com>
2026-06-25 15:27:36 -0500

    feat: configurar environments con API URL

--- helpdesk-frontend/src/environments/environment.ts
+++ helpdesk-frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://api.tu-produccion.com/api/' // Reemplaza con la URL real de tu backend en producción
+  apiUrl: 'https://tu-backend.onrender.com/api'
 };
